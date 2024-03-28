2f12949a14e082902ee305ebde6dcda379abafc0
Raghav Dixit <114759953+raghav01012004@users.noreply.github.com>
2024-03-28 19:32:19 +0530

    Edited Registration page

--- src/screens/RegisterScreen.jsx
+++ src/screens/RegisterScreen.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "../styles/regscreen.css";
 import { useNavigate } from "react-router-dom";
+import car from '../assets/mcqueen.png'
+import { ReactTyped } from "react-typed";
 
 const RegisterScreen = () => {
   const navigate = useNavigate();
@@ -12,12 +14,33 @@ const RegisterScreen = () => {
   };
   return (
     <div className="container">
-      <div className="app-image"></div>
+      
+    <br />
+
+    {/* <ReactTyped
+      strings={[
+        "Search for products",
+        "Search for categories",
+        "Search for brands",
+      ]}
+      typeSpeed={40}
+      backSpeed={50}
+      attr="placeholder"
+      loop
+    >
+      <input type="text" />
+    </ReactTyped> */}
+
+      <div className="app-image">
+      <img src={car} alt="" data-aos="slide-left"/>
+      <h1><ReactTyped strings={["BlockChain Powered Car Pooling", "A Revolution in the world of Car Pooling","'Decentralization' for Trust and Security","Ensuring money security","Secure payments b/w users"]} typeSpeed={20} backSpeed={10} loop/></h1>
+      </div>
       <div className="app-options">
-        <div className="wlc-msg">
+        <div className="wlc-msg" data-aos="fade-up">
           <h1 className="poppins-regular">Welcome to Carpooling</h1>
+          {/* <ReactTyped strings={["Welcome to Carpooling"]} typeSpeed={40} backSpeed={10}/> */}
         </div>
-        <div className="btns">
+        <div className="btns" data-aos="fade-down">
           <button
             className="signInbtn btn poppins-regular"
             onClick={onClickSignIn}
