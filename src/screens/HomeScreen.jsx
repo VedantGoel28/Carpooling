3e7be5c81527e588b741ec1c2ad20a8f86b6633b
MCUxKILLER <svgamer7781@gmail.com>
2024-03-29 04:00:58 +0530

    add metamask check

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,17 +1,45 @@
 import React from "react";
 import "../styles/homescreen.css";
-import { UserButton } from "@clerk/clerk-react";
+import { Link } from "react-router-dom";
+import { UserButton, useUser } from "@clerk/clerk-react";
 const HomeScreen = () => {
+  const { user } = useUser();
+  const primaryWeb3Wallet = user?.primaryWeb3Wallet;
+  const checkIfUserHasMetamask = () => {
+    if (
+      primaryWeb3Wallet === "" ||
+      primaryWeb3Wallet === null ||
+      primaryWeb3Wallet === undefined
+    ) {
+      return (
+        <div className="meta-msg">
+          <span>Metamask not connected. Please connect to metamask.</span>
+          <Link>Connect metamask</Link>
+        </div>
+      );
+    }
+  };
   return (
     <div className="home-container">
       <div className="user-button-container">
-        <UserButton showName={true} afterSignOutUrl="/" />
+        <UserButton
+          showName={true}
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              userButtonOuterIdentifier: {
+                color: "white",
+              },
+            },
+          }}
+        />
+        {checkIfUserHasMetamask()}
       </div>
       <div className="homescreen-body">
-        <h1>This is the home screen</h1>
+        <h1>This is the home screen </h1>
       </div>
     </div>
-  );m
+  );
 };
 
 export default HomeScreen;
