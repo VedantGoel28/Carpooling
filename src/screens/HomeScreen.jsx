76ddc867efc167c69088574fe99cae860caa7dc2
Raghav Dixit <114759953+raghav01012004@users.noreply.github.com>
2024-04-10 01:09:06 +0530

    added navbar to home screen

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -4,14 +4,17 @@ import { UserButton } from "@clerk/clerk-react";
 const HomeScreen = () => {
   return (
     <div className="home-container">
-      <div className="user-button-container">
-        <UserButton showName={true} afterSignOutUrl="/" />
+      <div class="navbar">
+        <div class="company-name">Chain Ride</div>
+        <div className="user-button-container">
+          <UserButton showName={true} afterSignOutUrl="/" />
+        </div>
       </div>
       <div className="homescreen-body">
         <h1>This is the home screen</h1>
       </div>
     </div>
-  );m
+  ); m
 };
 
 export default HomeScreen;
