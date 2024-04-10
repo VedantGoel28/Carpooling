9c26ca0a62e880a65c93bf8acd97df7b0d29c27f
Raghav Dixit <114759953+raghav01012004@users.noreply.github.com>
2024-04-11 01:54:25 +0530

    added images to carousel

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import "../styles/homescreen.css";
 import { Link } from "react-router-dom";
 import { UserButton, useUser } from "@clerk/clerk-react";
+import img1 from "../assets/c1.png";
+import img2 from "../assets/c2.png";
+import img3 from "../assets/c3.png";
+import img4 from "../assets/c4.png";
+import img5 from "../assets/c5.png"
 const HomeScreen = () => {
   const { user } = useUser();
   const primaryWeb3Wallet = user?.primaryWeb3Wallet;
@@ -19,26 +24,105 @@ const HomeScreen = () => {
       );
     }
   };
+
+  let slideIndex = 0;
+
+  function showSlides() {
+    const slides = document.querySelectorAll('.carousel-item');
+    if (slideIndex >= slides.length) {
+      slideIndex = 0;
+    } else if (slideIndex < 0) {
+      slideIndex = slides.length - 1;
+    }
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'none';
+    }
+    slides[slideIndex].style.display = 'block';
+  }
+
+  // function nextSlide() {
+  //   slideIndex++;
+  //   showSlides();
+  // }
+
+  // function prevSlide() {
+  //   slideIndex--;
+  //   showSlides();
+  // }
+
+  // Automatic rotation
+  setInterval(() => {
+    slideIndex++;
+    showSlides();
+  }, 5000);
+
+
   return (
     <div className="home-container">
       <div className="navbar">
-      <div class="company-name">Ether Shuttle</div>
-      <div className="user-button-container">
-        <UserButton
-          showName={true}
-          afterSignOutUrl="/"
-          appearance={{
-            elements: {
-              userButtonOuterIdentifier: {
-                color: "white",
-                fontSize: "2rem"
+        <div class="company-name" data-aos="slide-left">Ether Shuttle</div>
+        <div className="user-button-container" data-aos="slide-right">
+          <UserButton
+            showName={true}
+            afterSignOutUrl="/"
+            appearance={{
+              elements: {
+                userButtonOuterIdentifier: {
+                  color: "white",
+                  fontSize: "2rem"
+                },
               },
-            },
-          }}
-        />
-        {checkIfUserHasMetamask()}
+            }}
+          />
+          {checkIfUserHasMetamask()}
+        </div>
       </div>
+
+      <div class="carousel" id="myCarousel">
+        <div class="carousel-inner" >
+          <div class="carousel-item active" data-aos="fade-up">
+            <img src={img1} alt="Image 1" />
+            <div class="carousel-caption">
+            <h4>BlockChain Powered Car Pooling</h4>
+            <h6>A Revolution in the world of Car Pooloing.</h6>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src={img2} alt="Image 2" />
+            <div class="carousel-caption">
+            <h4>Multiple Upsides</h4>
+          <h6>The project comes with a wide variety of both <br /> Environmental & Technological advantages.</h6>
+
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src={img3} alt="Image 3" />
+            <div class="carousel-caption">
+            <h4>Decentralization</h4>
+          <h6>Blockchain comes with the feature of 'Decentralization'<br /> enhancing Trust and Security.</h6>
+
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src={img4} alt="Image 4" />
+            <div class="carousel-caption">
+            <h4>Smart Contracts</h4>
+          <h6>Ensuring money security and secure & seamless payments b/w users.</h6>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src={img5} alt="Image 5" />
+            <div class="carousel-caption">
+            <h4>Join Us!!</h4>
+          <h6>Become a part of our family and revolutionize car-pooling.</h6>
+            </div>
+          </div>
+        </div>
+
+        {/* <button class="carousel-btn prev-btn" onClick="prevSlide()">&#10094;</button>
+        <button class="carousel-btn next-btn" onClick="nextSlide()">&#10095;</button> */}
       </div>
+
       <div className="homescreen-body">
         <h1>This is the home screen </h1>
       </div>
