f423dd99540748ce774b0c830ac7c235c38da666
MCUxKILLER <svgamer7781@gmail.com>
2024-06-06 00:38:31 +0530

    deployment changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "https://peer-to-peer-carpooling.onrender.com",
+    port: process.env.PORT || 10000,
+  }
 })
